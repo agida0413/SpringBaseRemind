7bc6140441ea5d6feb8ead537e8160bbab0b5617
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-07-26 19:54:18 +0900

    123

---
+++ Step4/src/main/java/com/sist/SpringConfig.java
@@ -0,0 +1,25 @@
+package com.sist;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.sist.respository.MemberRepository;
+import com.sist.respository.MemoryMemberRepository;
+import com.sist.service.MemberService;
+
+@Configuration
+public class SpringConfig {
+// 
+	@Bean
+	public MemberService memberService() {
+	
+		return new MemberService(memberRepository());
+	
+	}
+	
+	@Bean
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+	
+}
---
+++ Step4/src/test/java/com/sist/Step2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
