22782660866a6b26aee7b662db92f2120b4d23fa
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-07-27 15:30:34 +0900

    2024/07/27

---
+++ Step6/src/main/java/com/example/demo/Step6Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step6Application.class, args);
+	}
+
+}
